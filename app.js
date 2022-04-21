dfd6e0fc3463190175f9844ee1526b6127b5fdbd
Jose Garcia <jogarcia@tuta.io>
2022-04-21 13:43:11 -0400

    Import body-parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 const { sequelize, Item } = require("./db-connection");
