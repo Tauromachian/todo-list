b46d9f28077d32d3264035fc9dc7fcf4c0873384
Jose Garcia <jogarcia@tuta.io>
2022-04-21 13:43:20 -0400

    Add new routes

--- routes.js
+++ routes.js
@@ -1,13 +1,17 @@
 module.exports = { route };
 
-function route(app) {
+function route(app, Item) {
   app.get("/", (req, res) => {
-    res.render("index", { message: "some message" });
+    Item.findAll().then((items) => res.render("index", { items: items }));
+  });
+  app.get("/new-item", (req, res) => {
+    res.render("new-item");
+  });
+  app.post("/item", (req, res) => {
+    const newItem = { ...req.body };
+    Item.create(newItem).then(() => res.redirect("/"));
   });
   app.get("/about", (req, res) => {
-    res.render("about");
-  });
-  app.post("/about", (req, res) => {
     res.render("about");
   });
 }
