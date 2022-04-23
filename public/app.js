71161f0c99ccfe94ae306efd84bba6f4960729b4
Hisham Mohammed <hish1993@googlemail.com>
2022-04-23 20:06:54 +0100

    adding delete functionality

--- public/app.js
+++ public/app.js
@@ -1,1 +1,7 @@
 document.body.style.background = "black";
+
+function deleteTodo(id){
+    console.log("hello")
+    fetch("/delete/"+id, {method: 'delete'})
+    document.getElementById(id).remove()
+}
--- routes.js
+++ routes.js
@@ -7,6 +7,11 @@ function route(app, Item) {
   app.get("/new-item", (req, res) => {
     res.render("new-item");
   });
+  app.delete("/delete/:id", (req, res) => {
+    console.log(req.params.id)
+    Item.destroy({where: { id: req.params.id }}).then(() => res.redirect("/"))
+    console.log("item deleted")
+  });
   app.post("/item", (req, res) => {
     const newItem = { ...req.body };
     Item.create(newItem).then(() => res.redirect("/"));
